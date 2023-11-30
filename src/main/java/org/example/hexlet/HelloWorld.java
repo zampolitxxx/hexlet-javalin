426d65d41fe8c46ef5eb509fca01f9cf8c67c3ce
zampolitxxx <zampolitxxx@gmail.com>
2023-11-30 14:25:29 +0900

    initializing project

---
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.example.hexlet;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Создаем приложение
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        app.get("/", ctx -> ctx.result("Hello World"));
+        app.start(7070); // Стартуем веб-сервер
+    }
+}
