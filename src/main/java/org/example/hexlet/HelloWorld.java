e4e781287ff46b815a691986c8881aba9eab3079
zampolitxxx <zampolitxxx@gmail.com>
2024-01-01 16:33:24 +0900

    add named routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,19 +2,15 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 import io.javalin.validation.ValidationException;
-import org.apache.commons.text.StringEscapeUtils;
 import org.example.hexlet.dto.courses.BuildCoursePage;
-import org.example.hexlet.dto.courses.CoursePage;
-//import org.example.hexlet.dto.courses.CoursePage;
-//import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.dto.courses.CoursesPage;
 import org.example.hexlet.dto.users.BuildUserPage;
-import org.example.hexlet.dto.users.UserPage;
 import org.example.hexlet.dto.users.UsersPage;
 import org.example.hexlet.model.Course;
 import org.example.hexlet.model.User;
 import org.example.hexlet.repository.CourseRepository;
 import org.example.hexlet.repository.UserRepository;
+import org.example.hexlet.util.NamedRoutes;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,12 +26,12 @@ public class HelloWorld {
         });
 
         //show all courses
-        app.get("/courses", ctx -> {
+        app.get(NamedRoutes.getCoursesPath(), ctx -> {
             var page = new CoursesPage(CourseRepository.getEntities());
             ctx.render("courses/index.jte", Collections.singletonMap("page", page));
         });
         //POST-handler to create course
-        app.post("/courses", ctx -> {
+        app.post(NamedRoutes.getCoursesPath(), ctx -> {
             try{
                 var name = ctx.formParamAsClass("name", String.class)
                         .check(value -> value.length() > 2, "Length should be more then 2 simbols")
@@ -45,25 +41,25 @@ public class HelloWorld {
                         .get();
                 var course = new Course(name, description);
                 CourseRepository.addCourse(course);
-                ctx.redirect("/courses");
+                ctx.redirect(NamedRoutes.getCoursesPath());
             } catch(ValidationException e) {
                 var page = new BuildCoursePage(e.getErrors());
                 ctx.render("courses/buildCourse.jte", Collections.singletonMap("page", page));
             }
         });
         //GET-handler with HTML-form add course
-        app.get("/courses/build", ctx -> {
+        app.get(NamedRoutes.getBuildCoursePath(), ctx -> {
             var page = new BuildCoursePage();
             ctx.render("courses/buildCourse.jte", Collections.singletonMap("page", page));
         });
 
         //Show all users
-        app.get("/users", ctx -> {
+        app.get(NamedRoutes.getUsersPath(), ctx -> {
             var page = new UsersPage(UserRepository.getEntities());
             ctx.render("users/index.jte", Collections.singletonMap("page", page));
         });
         //POST-hendler to create user
-        app.post("/users", ctx -> {
+        app.post(NamedRoutes.getUsersPath(), ctx -> {
             var name = ctx.formParam("name");
             var surname = ctx.formParam("surname");
             var email = ctx.formParam("email");
@@ -75,7 +71,7 @@ public class HelloWorld {
                         .get();
                 var user = new User(name, surname, email, passwordConfirm);
                 UserRepository.addUser(user);
-                ctx.redirect("/users");
+                ctx.redirect(NamedRoutes.getUsersPath());
             } catch (ValidationException e) {
                 var page = new BuildUserPage(name, surname, email, e.getErrors());
                 ctx.render("users/buildUser.jte", Collections.singletonMap("page", page));
@@ -83,7 +79,7 @@ public class HelloWorld {
 
         });
         //Get-hendler with HTML-form
-        app.get("/users/build", ctx -> {
+        app.get(NamedRoutes.getBuildUserPath(), ctx -> {
             var page = new BuildUserPage();
             ctx.render("users/buildUser.jte", Collections.singletonMap("page", page));
         });
---
+++ src/main/java/org/example/hexlet/util/NamedRoutes.java
@@ -0,0 +1,20 @@
+package org.example.hexlet.util;
+
+public class NamedRoutes {
+    public static String getRootPath() {
+        return "/";
+    }
+    public static String getCoursesPath() {
+        return "/courses";
+    }
+    public static String getBuildCoursePath() {
+        return "/courses/build";
+    }
+
+    public static String getUsersPath() {
+        return "/users";
+    }
+    public static String getBuildUserPath() {
+        return "/users/build";
+    }
+}
